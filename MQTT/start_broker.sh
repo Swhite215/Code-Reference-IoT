756d938298f50c79e0bbf69ca0928508cd1876da
Spencer White <swhit210@ford.com>
2021-02-24 11:41:15 -0500

    added option to start hivemq broker

--- MQTT/start_broker.sh
+++ MQTT/start_broker.sh
@@ -2,6 +2,8 @@ if [ $1 -eq 1 ]; then
     brew services start mosquitto
 elif [ $1 -eq 2 ]; then
     /usr/local/sbin/mosquitto -c /usr/local/etc/mosquitto/mosquitto.conf
+elif [ $1 -eq 3 ]; then
+    /usr/local/bin/hivemq-4.5.1/bin/run.sh
 else
     echo "MQTT Broker not started..."
     exit
